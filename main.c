860f967101a72b0717f78e9f2528200860ff3de0
zohran21 <maxence.poisson27@live.fr>
2017-12-12 09:50:26 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,29 +6,59 @@
 int main()
 {
     FILE *fichier = NULL;
-    ouvrirFichier(fichier);
-
+    char[] fichierEntier = ouvrirFichier(fichier);
+	char[][] tabLine = SplitFile(fichierEntier);
+	
     return 0;
 }
 
-void ouvrirFichier(FILE *fichier) {
+char[] ouvrirFichier(FILE *fichier) {
     fichier = fopen("coty.csv", "r+");
-
     if (fichier != NULL)
+
     {
-        // On peut lire et écrire dans le fichier
+        // On peut lire et Ã©crire dans le fichier
         printf("ouverture du fichier reussie");
-        //fputc('B', fichier); // Écriture du caractère A
+        //fputc('B', fichier); // Ã‰criture du caractÃ¨re A
     }
     else
     {
         // On affiche un message d'erreur si on veut
         printf("Impossible d'ouvrir le fichier coty.csv");
     }
-    int caraActuel;
+    char[] fichierEntier = "";
     do {
-        caraActuel = fgetc(fichier);
-        printf("%c", caraActuel);
+        fichierEntier += fgetc(fichier);
     } while (caraActuel != EOF);
 
+}
+
+char[][] SplitFile(char[] fichierEntier)
+{
+	int lenght = ft_strlen(fichierEntier);
+	char[][] tabSortie;
+	int i;
+	int x = 0;
+	int y = 0;
+	for( i=0; i < lenght; i++)
+	{
+		while (fichierEntier[i] != '\0')
+		{
+			tabSortie[x][y] = fichierEntier[i];
+			y++;
+		}
+		y = 0;
+		x++;
+	}
+	return tabSortie;
+}
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
